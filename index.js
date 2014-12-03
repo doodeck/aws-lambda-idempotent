87e93e909b24c6a0c34a756619ee13902b9b213f
Kris Dudkiewicz <doodeck@users.noreply.github.com>
2014-12-03 19:14:44 +0100

    Implemented workaround to prevent multiple function invocations cumulating memory leaks up to the point they were effectively crashing

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ var config = {
     functionName: 'invokeIdempotent',
     region: 'eu-west-1',
     idmptTableName: 'LambdaLocks',
-    timeout: 5000
+    timeout: 5000,
+    restartInterval: 100, // after how many invocations should the restart be forced
+    lambdaTimeout: 16 // default value of the handler
 };
 
 // createTable.js
@@ -89,7 +91,10 @@ var lambdaIdempotent = function(params, callback) {
                 };
                 console.log('Next gen: ', invokeSyncParams);           // successful response
                 // params.ExpectedSeq++;
-                lambda.invokeAsync(invokeSyncParams, callback);
+                lambda.invokeAsync(invokeSyncParams, function(err, data) {
+                    console.log('lambda.invokeAsync: ', err, data);
+                    callback(err, data);
+                });
             }
         });
     } catch(e) {
@@ -97,6 +102,37 @@ var lambdaIdempotent = function(params, callback) {
         callback(e, {});
     }
 };
+
+// perform an innocent function configuration change to prevent reusing the same runtime (memory leak workaround)
+// for details see the following thread:
+// https://forums.aws.amazon.com/message.jspa?messageID=587439
+var forceRestart = function(callback) {
+    var params = {
+        FunctionName: config.functionName /* required */
+    };
+    lambda.getFunctionConfiguration(params, function(err, data) {
+        if (err) {
+            console.log(err, err.stack); // an error occurred
+            callback(err, data);
+        } else {
+            // console.log(data);           // successful response
+            var newTimeout = config.lambdaTimeout === data.Timeout ? config.lambdaTimeout + 1 : config.lambdaTimeout;
+            var params = {
+              FunctionName: config.functionName, /* required */
+              Timeout: newTimeout
+            };
+            lambda.updateFunctionConfiguration(params, function(err, data) {
+                if (err) {
+                    console.log(err, err.stack); // an error occurred
+                    callback(err, data);
+                } else {
+                    console.log('Successfully updated timeout: ', data); // successful response
+                    callback(err, data);
+                }
+            });
+        }
+    });
+}
 
 // invokeIdempotent.js
 var vanillaTest;
@@ -120,10 +156,15 @@ exports.invokeIdempotent = function(event, context) {
     console.log('On entry: ', params);
     /*lambda.invokeAsync(params, function(err, data) {*/
     lambdaIdempotent(params, function(err, data) {
-      if (err) {
-          console.log('lambdaIdempotent failed: ', err);
-      }
-      context.done(null, 'Exiting InstanceId: ' + event.InstanceId + ', ExpectedSeq: ' + event.ExpectedSeq.toString());  // SUCCESS with message
+        if (err) {
+            console.log('lambdaIdempotent failed: ', err);
+        }
+        if (event.ExpectedSeq % config.restartInterval === 0)
+            forceRestart(function(err, data) {
+              context.done(null, 'Restarted InstanceId: ' + event.InstanceId + ', ExpectedSeq: ' + event.ExpectedSeq.toString());  // SUCCESS with message
+            });
+        else
+            context.done(null, 'Exiting InstanceId: ' + event.InstanceId + ', ExpectedSeq: ' + event.ExpectedSeq.toString());  // SUCCESS with message
     });
   }, config.timeout);
 };
