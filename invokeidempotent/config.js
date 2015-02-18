381f85e24f98e13137ff73c2dd27de25b5b591e7
Kris Dudkiewicz <doodeck@users.noreply.github.com>
2015-02-18 07:31:00 +0100

    Added a new way to start the function without manual preparations

--- invokeidempotent/config.js
+++ invokeidempotent/config.js
@@ -10,6 +10,7 @@ exports.config = {
     timeoutDev: 2000, // random deviation from the timeout (to eliminate multiple instances accidentally running in parallel)
     restartInterval: 200, // after how many invocations should the restart be forced
     lambdaTimeout: 16, // default value of the handler
-    acceptRange: 2 // range of accepted values in DynamoDB, in other words: retry count,
+    acceptRange: 2, // range of accepted values in DynamoDB, in other words: retry count,
                    // should help in case Dynamo is updated but then the function freezes and the framework starts it again
+    initialExpectedSeq: 1 // initial ExpectedSeq for cases when the DynamoDB item is to be created
 };
--- invokeidempotent/invokeidempotent.js
+++ invokeidempotent/invokeidempotent.js
@@ -95,40 +95,88 @@ var forceRestart = function(callback) {
     });
 }
 
+// retrieve params for lambdaIdempotent either from event/context or my creating a new DynamoDB entry
+var getParams = function(event, context, callback) {
+  var params = {
+    InvokeId: context.invokeid
+  };
+  if (!!event.InstanceId && !!event.ExpectedSeq) {
+    params.InstanceId = event.InstanceId;
+    params.ExpectedSeq = event.ExpectedSeq;
+    callback(undefined /*err*/, params);
+  } else {
+    params.InstanceId = context.invokeid; // initialize it to unique value, not necessaily invokeid
+    params.ExpectedSeq = config.initialExpectedSeq;
+
+    // create the new item in the DB
+    try {
+        var item = {
+          Item: { /* required */
+            FunctionName: {
+              S: config.functionName.toString()
+            },
+            InstanceId: {
+              S: params.InstanceId.toString()
+            },
+            InvokeId: {
+              S: params.InvokeId.toString()
+            },
+            Seq: {
+              N: params.ExpectedSeq.toString()
+            }
+          },
+          TableName: config.idmptTableName
+        };
+        console.log('putItem item: ', item);
+        dynamodb.putItem(item, function(err, data) {
+          if (err) {
+            console.log(err, err.stack); // an error occurred
+            callback(err, {});
+          } else {
+            callback(undefined, params);
+          }
+        });
+    } catch(e) {
+        console.log('putItem exception: ', e);
+        callback(e, {});
+    }
+  }
+}
+
 // invokeIdempotent.js
 var vanillaTest;
 
 /* invocation event:
+Now it's possible to invoke the function in two ways:
+1. With the InstanceId/ExpectedSeq set like here:
 {
     "InstanceId": "<instance_id_aka_groups_id>",
     "ExpectedSeq": <expected_seq>
 }
+ Then you must manually create a corresponding DynamoDB entry with matching values. Or
+ 2. Without the above field, e.g. with {} as a parameter. Then the corresponding DynamoDB entry is created
+    automatically. Sure, you can create multiple function instances in this way, but you will easily notice it
+    by scanning through the LambdaLocks table.
 */
 exports.handler = function(event, context) {
   // vanillaTest(event,context);
   var randomizedTimeout = Math.floor((Math.random() * config.timeoutDev) + config.timeout);
   console.log('Using timeout: ', randomizedTimeout);
   setTimeout(function() {
-    var params = {
-      // FunctionName: config.functionName,
-      InstanceId: event.InstanceId, // multiple instances of the same function can be running simultaneously in different groups
-      ExpectedSeq: event.ExpectedSeq, // Seq value currently in the database. If function is unable to retrieve and increment that value the lamdba recursive invocation will not take place
-      InvokeId: context.invokeid
-      // InvokeArgs: '{}'
-    };
-
-    console.log('On entry: ', params);
-    /*lambda.invokeAsync(params, function(err, data) {*/
-    lambdaIdempotent(params, function(err, data) {
-        if (err) {
-            console.log('lambdaIdempotent failed: ', err);
-        }
-        if (event.ExpectedSeq % config.restartInterval === 0)
-            forceRestart(function(err, data) {
-              context.done(null, 'Restarted InstanceId: ' + event.InstanceId + ', ExpectedSeq: ' + event.ExpectedSeq.toString());  // SUCCESS with message
-            });
-        else
-            context.done(null, 'Exiting InstanceId: ' + event.InstanceId + ', ExpectedSeq: ' + event.ExpectedSeq.toString());  // SUCCESS with message
+    getParams(event, context, function(err, params) {
+      console.log('gotParams: ', params);
+      /*lambda.invokeAsync(params, function(err, data) {*/
+      lambdaIdempotent(params, function(err, data) {
+          if (err) {
+              console.log('lambdaIdempotent failed: ', err);
+          }
+          if (event.ExpectedSeq % config.restartInterval === 0)
+              forceRestart(function(err, data) {
+                context.done(null, 'Restarted InstanceId: ' + params.InstanceId + ', ExpectedSeq: ' + params.ExpectedSeq.toString());  // SUCCESS with message
+              });
+          else
+              context.done(null, 'Exiting InstanceId: ' + params.InstanceId + ', ExpectedSeq: ' + params.ExpectedSeq.toString());  // SUCCESS with message
+      });
     });
   }, randomizedTimeout);
 };
