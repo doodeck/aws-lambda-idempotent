a48de4692c55b9f7d5723ebee964f1f4ba67b7ec
Kris Dudkiewicz <doodeck@users.noreply.github.com>
2015-03-01 21:35:41 +0100

    Removed the very controversial content in payload

--- invokeidempotent/modules/payload.js
+++ invokeidempotent/modules/payload.js
@@ -4,8 +4,13 @@ exports.handler = function() {
   var startingMoment = +new Date();
   console.log('payload: ', new Date(startingMoment).toString());
   var index = 0;
+  /*
+  This is very special payload, see the project:
+  https://github.com/doodeck/life-after-life
+  to understand the consequences
   setInterval(function() {
     var delta = +new Date() - startingMoment;
     console.log('index: ', index++, new Date(delta).toString());
   }, (10 * 100));
+  */
 }
