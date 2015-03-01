c334fdd08805fa2f4d84b0c806bd3797d1075dab
Kris Dudkiewicz <doodeck@users.noreply.github.com>
2015-03-01 21:51:11 +0100

    Updated the deployment script after having been moved

--- upload-function.sh
+++ upload-function.sh
@@ -1,3 +1,4 @@
 #
 
-zip -r tmp/index.zip *.js modules/ node_modules/ ; aws --profile lambda lambda upload-function --region eu-west-1 --function-name invokeidempotent --function-zip tmp/index.zip  --role 'arn:aws:iam::915133436062:role/lambda_exec_role' --mode event --handler invokeidempotent.handler --runtime nodejs --timeout 17
+mkdir -p tmp
+zip -r tmp/index.zip invokeidempotent/*.js invokeidempotent/modules/ node_modules/ ; aws --profile lambda lambda upload-function --region eu-west-1 --function-name invokeidempotent --function-zip tmp/index.zip  --role 'arn:aws:iam::915133436062:role/lambda_exec_role' --mode event --handler invokeidempotent/invokeidempotent.handler --runtime nodejs --timeout 17
