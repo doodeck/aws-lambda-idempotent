f6864f57bbf3439b86dbf0fa2774d8dfb512142e
Kris Dudkiewicz <doodeck@users.noreply.github.com>
2015-03-26 07:18:13 +0100

    Added important timeout, i.e. restart, information

--- invokeidempotent/config.js
+++ invokeidempotent/config.js
@@ -9,7 +9,7 @@ exports.config = {
     timeout: 4000, // timeout before function is invoked and content really executed
     timeoutDev: 2000, // random deviation from the timeout (to eliminate multiple instances accidentally running in parallel)
     restartInterval: 200, // after how many invocations should the restart be forced
-    lambdaTimeout: 16, // default value of the handler
+    lambdaTimeout: 16, // // you dare not make it 60, or the forced restart mechanism will not work! default value of the handler
     acceptRange: 2, // range of accepted values in DynamoDB, in other words: retry count,
                    // should help in case Dynamo is updated but then the function freezes and the framework starts it again
     initialExpectedSeq: 1, // initial ExpectedSeq for cases when the DynamoDB item is to be created
--- upload-function.sh
+++ upload-function.sh
@@ -2,3 +2,4 @@
 
 mkdir -p tmp
 zip -r tmp/index.zip invokeidempotent/*.js invokeidempotent/modules/ node_modules/ ; aws --profile lambda lambda upload-function --region eu-west-1 --function-name invokeidempotent --function-zip tmp/index.zip  --role 'arn:aws:iam::915133436062:role/lambda_exec_role' --mode event --handler invokeidempotent/invokeidempotent.handler --runtime nodejs --timeout 17
+# Keep the timeout value in sync with the config.js
