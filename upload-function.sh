f75864672b83e8e57e78d50753b3968ea1c5f8a9
Kris Dudkiewicz <doodeck@users.noreply.github.com>
2015-05-03 09:07:01 +0200

    As of AWS CLI 1.7 upload-function option has been discontinued

--- upload-function.sh
+++ upload-function.sh
@@ -1,5 +1,8 @@
 #
 
 mkdir -p tmp
-zip -r tmp/index.zip invokeidempotent/*.js invokeidempotent/modules/ node_modules/ ; aws --profile lambda lambda upload-function --region eu-west-1 --function-name invokeidempotent --function-zip tmp/index.zip  --role 'arn:aws:iam::915133436062:role/lambda_exec_role' --mode event --handler invokeidempotent/invokeidempotent.handler --runtime nodejs --timeout 17
+zip -r tmp/index.zip invokeidempotent/*.js invokeidempotent/modules/ node_modules/
+# As of CLI 1.7 upload-function option has been discontinued. Use update-function-code instead
+# aws --profile lambda lambda upload-function --region eu-west-1 --function-name invokeidempotent --function-zip tmp/index.zip  --role 'arn:aws:iam::915133436062:role/lambda_exec_role' --mode event --handler invokeidempotent/invokeidempotent.handler --runtime nodejs --timeout 17
 # Keep the timeout value in sync with the config.js
+aws --profile lambda lambda update-function-code --region eu-west-1 --function-name invokeidempotent --zip-file fileb://tmp/index.zip
